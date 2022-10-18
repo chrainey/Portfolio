c303f65488388125c1e8a518a9944da175d64d8d
Chris Rainey <chrainey@hotmail.co.uk>
2022-10-18 11:35:42 +0100

    Added project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,6 @@ import projImg2 from "../assets/images/Project2image.png"
 import projImg3 from "../assets/images/Project3image.png"
 import projImg4 from "../assets/images/Project4image.png"
 import github from "../assets/images/github.jpeg"
-import readme from "../assets/images/readme.webp"
 import chain from "../assets/images/link.png"
 
 
@@ -17,21 +16,29 @@ const projects = [
     title: "Football Legends App",
     description: "For this project I built my own full-stack app using a Python Django API to serve my data from a Postgres database. The frontend was built using React. It has CRUD functionality with OnetoMany and ManytoMany relationships. Cloudinary was used to store all of the images. I created a football app exploring some of the greatest players and clubs in european football.",
     imgUrl: projImg4,
+    link: 'https://footballs-greatest.herokuapp.com/',
+    gitLink: 'https://github.com/chrainey/SEI-Project-4-GreatestPlayers',
+    
   },
   {
     title: "Travel Destinations App",
     description: "For this project, in a team using GitHub, we built our own API and front end website as a Travel Database app. This included user and authentication features using JSL tokens. We also used AWS and Cloudinary to store images for the database. I was responsible for the look and styling off the app along with building many of the frontend components.",
     imgUrl: projImg3,
+    link: 'https://sei65-destinations.netlify.app/',
   },
   {
     title: "Cocktail Recipe Database",
     description: "We built a React application that consumes and uses a public API. I used Insomnia to test out GET and POST API requests and this also taught me how to useState and useEffect within an application. Using API requests we created a slick CSS/SASS designed app that would bring up cocktails recipes by selecting various alcoholic ingredients.",
     imgUrl: projImg2,
+    link: 'https://cocktail-library.netlify.app',
+    gitLink: 'https://github.com/chrainey/SEI-Project-2-CocktailAPI',
   },
   {
     title: "Space Invaders Game",
     description: "For this project I built a Space Invaders JavaScript grid based computer game. Using pure JavaScript, without a HTML canvas, this was styled with CSS.",
     imgUrl: projImg1,
+    link: 'https://chrainey.github.io/SEI-Project-1-SpaceInvaders/',
+    gitLink: 'https://github.com/chrainey/SEI-Project-1-SpaceInvaders',
   },
 ];
 
@@ -51,9 +58,8 @@ const Projects = () => {
                 </Card.Text>
               </Card.Body>
               <Card.Body>
-                <Card.Link href="#"><img src={chain} class='project-icons' alt="" /></Card.Link>
-                <Card.Link href="#"><img src={github} class='project-icons' alt="" /></Card.Link>
-                <Card.Link href="#"><img src={readme} class='project-icons' alt="" /></Card.Link>
+                <Card.Link href={projects.link}><img src={chain} class='project-icons' alt="" /></Card.Link>
+                <Card.Link href={projects.gitLink}><img src={github} class='project-icons' alt="" /></Card.Link>
               </Card.Body>
             </Card>
           </Col>
