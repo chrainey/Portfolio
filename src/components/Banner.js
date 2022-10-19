8e93cf031588702bb6e2b133b27bdea15eaad7b4
Chris Rainey <chrainey@hotmail.co.uk>
2022-10-19 10:29:37 +0100

    Further Styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,15 +11,10 @@ const Banner = () => {
             <h1>{`Hi, my name is Chris`}</h1>
             <span className="wrap">I have recently trained to become a Junior Software Engineer</span>
             <p>I have 20 years experience in the customer service sector which, until recently, took
-              me as far as living in Italy. I was working managing holiday reps on the North Italian
-              lakes in the summer and looking after large teams and holidaymakers in Italy's ski
-              resorts. It was a gorgeous place to work!! Unfortunately, the combination of Brexit and Covid meant I had to return to
-              the UK. I went back into the hospitality sector as I know how to manage people and
-              deal with customers but I needed something to change my life.
+              me as far as living in Italy. It was a gorgeous place to work!! Unfortunately, the combination of Brexit and Covid meant I had to return to
+              the UK. 
               So I decided to take a immersive full-stack software engineering course inspired by
-              my interest in technology and the internet. I've always wanted to know how digital
-              systems and tools communicate with each other to make amazing things that we
-              take for granted every day. I am looking for somewhere that I can take my recent
+              my interest in technology and the internet.  I am looking for somewhere that I can take my recent
               education and build on it, learning from others but also bringing my experience of
               working in a team and with clients.</p>
           </Col>
